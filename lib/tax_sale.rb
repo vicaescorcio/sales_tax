5e67639bf2756145402a3c0514ce01953c173e2d
vicaescorcio <vica.escorcio@gmail.com>
2025-01-26 08:40:51 +0000

    Add rspec configuration

---
+++ lib/tax_sale.rb
@@ -0,0 +1,1 @@
+class TaxSale; end
---
+++ spec/tax_sale_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true: true
+
+require_relative "../lib/tax_sale"
+
+describe TaxSale do
+  describe "#run" do
+    it do
+      puts "i am working"
+    end
+  end
+end
